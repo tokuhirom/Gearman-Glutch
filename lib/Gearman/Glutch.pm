d1388b8b0b34e998e012b7152801e7c183bc2387
tokuhirom <tokuhirom@gmail.com>
2012-09-29 07:30:38 +0900

    initial import

---
+++ lib/Gearman/Glutch.pm
@@ -0,0 +1,39 @@
+package Gearman::Glutch;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Gearman::Glutch - A module for you
+
+=head1 SYNOPSIS
+
+  use Gearman::Glutch;
+
+=head1 DESCRIPTION
+
+Gearman::Glutch is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
